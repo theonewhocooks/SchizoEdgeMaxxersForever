8c7a8f1b0d9b3138e929838370612c86e6dfe295
Walter Hart-well White <113389560+theonewhocooks@users.noreply.github.com>
2025-01-23 09:17:27 -0800

    dasdadasdwafaw

--- SchizoEdgeMaxxersForever/Assets/Scripts/PlayerController.cs
+++ SchizoEdgeMaxxersForever/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,14 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+	public KeyCode jumpkey = KeyCode.Space;
+	public float jumpForce;
+	public float jumpCooldown;
+	public float airMultiplier;
+	bool readyToJump;
+	public float playerHeight;
+	public LayerMask whatIsGround;
+	bool grounded;
 
 	public float moveSpeed;
 
@@ -19,6 +27,7 @@ public class PlayerController : MonoBehaviour
 	{
 		rb = GetComponent<Rigidbody>();
 		rb.freezeRotation = true;
+		ResetJump();
 	}
 
     private void Update()
@@ -35,13 +44,37 @@ public class PlayerController : MonoBehaviour
     {
 		horizontalInput = Input.GetAxisRaw("Horizontal");
 		verticalInput = Input.GetAxisRaw("Vertical");
+		if(Input.GetKey(jumpkey) && readyToJump && grounded)
+		{
+			readyToJump = false;
+
+			Jump();
+
+			Invoke(nameof(ResetJump), jumpCooldown);
+		}
     }
 
 	void MovePlayer()
 	{
 		moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-		rb.AddForce(moveDirection.normalized * moveSpeed * 10, ForceMode.Force);
 
+		if(grounded)
+			rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+		else if(!grounded)
+			rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+	}
+
+	private void Jump()
+	{
+		rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+		rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+	}
+	private void ResetJump()
+	{
+		readyToJump = true;
 	}
 
 }
