efd01dad93095af8ce96f89182728b0047aeafd6
Walter Hart-well White <113389560+theonewhocooks@users.noreply.github.com>
2025-01-28 09:17:41 -0800

    dasdadasdadwafwafasg

--- SchizoEdgeMaxxersForever/Assets/Scripts/gunmover.cs
+++ SchizoEdgeMaxxersForever/Assets/Scripts/gunmover.cs
@@ -1,80 +1,20 @@
-using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
-using UnityEngine.UIElements;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class gunmover : MonoBehaviour
 {
-	public KeyCode jumpkey = KeyCode.Space;
-	public float jumpForce;
-	public float jumpCooldown;
-	public float airMultiplier;
-	bool readyToJump;
-	public float playerHeight;
-	public LayerMask whatIsGround;
-	bool grounded;
-
-	public float moveSpeed;
-
-	public Transform orientation;
-
-	float horizontalInput;
-	float verticalInput;
-	Vector3 moveDirection;
-	Rigidbody rb;
-
-	void Start()
-	{
-		rb = GetComponent<Rigidbody>();
-		rb.freezeRotation = true;
-		ResetJump();
-	}
-
-    private void Update()
+    public Transform objectPosition;
+    // Start is called before the first frame update
+    void Start()
     {
-		MyInput();
+        
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void LateUpdate()
     {
-		MovePlayer();
+        transform.position = objectPosition.position;
+        transform.rotation = objectPosition.rotation;
     }
-
-    private void MyInput()
-    {
-		horizontalInput = Input.GetAxisRaw("Horizontal");
-		verticalInput = Input.GetAxisRaw("Vertical");
-		if(Input.GetKey(jumpkey) && readyToJump && grounded)
-		{
-			readyToJump = false;
-
-			Jump();
-
-			Invoke(nameof(ResetJump), jumpCooldown);
-		}
-    }
-
-	void MovePlayer()
-	{
-		moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-		if(grounded)
-			rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-		else if(!grounded)
-			rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-
-	}
-
-	private void Jump()
-	{
-		rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-		rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-
-	}
-	private void ResetJump()
-	{
-		readyToJump = true;
-	}
-
 }
