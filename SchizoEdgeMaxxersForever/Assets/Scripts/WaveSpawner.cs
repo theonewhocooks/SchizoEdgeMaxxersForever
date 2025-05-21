10c36c10389fbdaae9398121dd67d8c2e8fe2774
Walter Hart-well White <113389560+theonewhocooks@users.noreply.github.com>
2025-05-21 09:14:01 -0700

    dwadgfad

--- SchizoEdgeMaxxersForever/Assets/Scripts/WaveSpawner.cs
+++ SchizoEdgeMaxxersForever/Assets/Scripts/WaveSpawner.cs
@@ -1,97 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEditor.Rendering.PostProcessing;
 using UnityEngine;
-using UnityEngine.Animations;
+using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
-    public List<Enemy>  enemies = new List<Enemy>();
-    public int currWave;
-    public int waveValue;
-    public List<GameObject> enemiesToSpawn = new List<GameObject>();
-    public Transform spawnLocation;
-    public int waveDuration;
-    private float waveTimer;
-    private float spawnInterval;
-    private float spawnTimer;
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Enemy
     {
-       GenerateWave(); 
+        public GameObject enemyPrefab;
+        public int cost;
     }
 
-    // Update is called once per frame
+    public List<Enemy> enemies = new List<Enemy>();
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
+
+    public Transform spawnLocation;
+    public int waveDuration = 60;
+    public int maxWaves = 6;
+    public int currWave = 0;
+
+    public TextMeshProUGUI waveText;
+
+    private int waveValue;
+    private float spawnInterval;
+    private float spawnTimer;
+    private float waveCooldownTimer = 5f;
+    private bool waveInProgress = false;
+
+    void Start()
+    {
+        currWave = 0;
+        GenerateWave();
+    }
+
     void FixedUpdate()
     {
-        if(spawnTimer <=0)
+        if (waveInProgress)
         {
             if (enemiesToSpawn.Count > 0)
             {
-                GameObject enemyToSpawn = enemiesToSpawn[0];
-                enemiesToSpawn.RemoveAt(0);
-                Instantiate(enemyToSpawn, spawnLocation.position, Quaternion.identity);
-                spawnTimer = spawnInterval;
-                Debug.Log("Spawning enemy: " + enemiesToSpawn[0].name);
+                if (spawnTimer <= 0)
+                {
+                    Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
+                    enemiesToSpawn.RemoveAt(0);
+                    spawnTimer = spawnInterval;
+                }
+                else
+                {
+                    spawnTimer -= Time.fixedDeltaTime;
+                }
             }
             else
             {
-                waveTimer = 0;
+                waveInProgress = false;
+                waveCooldownTimer = 5f;
             }
         }
-        else
+        else if (currWave < maxWaves)
         {
-            spawnTimer-= Time.fixedDeltaTime;
-            waveTimer-= Time.fixedDeltaTime;
+            waveCooldownTimer -= Time.fixedDeltaTime;
+
+            if (waveCooldownTimer <= 0)
+            {
+                GenerateWave();
+            }
         }
     }
 
     public void GenerateWave()
     {
-        waveValue = Mathf.Max(currWave * 10, 1);
+        currWave++;
+        Debug.Log("Spawning Wave: " + currWave);
+
+        if (waveText != null)
+            waveText.text = "Wave " + currWave;
+
+        waveValue = Mathf.Max(currWave * 13, 1);
         GenerateEnemies();
-
-        spawnInterval = waveDuration / enemiesToSpawn.Count;
-        waveTimer = waveDuration;
-
-        Debug.Log("Generated enemies: " + enemiesToSpawn.Count);
 
         if (enemiesToSpawn.Count == 0)
         {
-            Debug.LogWarning("No enemies generated nigga!!!!");
-            return;    
+            Debug.LogWarning("No enemies generated for wave " + currWave);
+            return;
         }
+
+        spawnInterval = (float)waveDuration / enemiesToSpawn.Count;
+        spawnTimer = 0;
+        waveInProgress = true;
     }
 
     public void GenerateEnemies()
-    {   
+    {
         List<GameObject> generatedEnemies = new List<GameObject>();
-        while(waveValue>0)
-        {
-            int randEnemyId = Random.Range(0, enemies.Count);
-            int randEnemyCost = enemies[randEnemyId].cost;
+        int remainingValue = waveValue;
 
-            if(waveValue-randEnemyCost>=0)
-            {
-                generatedEnemies.Add(enemies[randEnemyId].enemyPrefab);
-                waveValue -= randEnemyCost;
-            }
-            else if(waveValue<=0)
-            {
+        while (remainingValue > 0)
+        {
+            List<Enemy> affordableEnemies = enemies.FindAll(e => e.cost <= remainingValue);
+
+            if (affordableEnemies.Count == 0)
                 break;
-            }
+
+            Enemy chosen = affordableEnemies[Random.Range(0, affordableEnemies.Count)];
+            generatedEnemies.Add(chosen.enemyPrefab);
+            remainingValue -= chosen.cost;
         }
+
         enemiesToSpawn.Clear();
         enemiesToSpawn = generatedEnemies;
     }
-
-
-}
-
-[System.Serializable]
-public class Enemy
- {
-    public GameObject enemyPrefab;
-     public int cost;
 }
